27479dd5f9f9ef172c2318254006a1d5326dfb5b
nome_usuario <email_ususario>
2025-01-20 11:00:51 -0300

    feat: create login and register page

---
+++ src/app/(public)/accounts/login/index.tsx
@@ -0,0 +1,72 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import {googleSvg} from "@/assets/svg";
+import Link from "next/link";
+
+export function LoginForm({
+                              className,
+                              ...props
+                          }: React.ComponentPropsWithoutRef<"div">) {
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Entrar</CardTitle>
+                    <CardDescription>
+                        Entre com seu email e senha para acessar a plataforma.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="me@exemplo.com"
+                                />
+                            </div>
+
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Senha</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Esqueceu sua senha?
+                                    </a>
+                                </div>
+                                <Input id="password" type="password" placeholder={"****************"} />
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Entrar
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                {googleSvg}
+                                Entrar com Google
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Não tem conta?{" "}
+                            <Link href="/register" className="underline underline-offset-4">
+                                Registrar-se
+                            </Link>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
---
+++ src/app/(public)/accounts/register/index.tsx
@@ -0,0 +1,80 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import {googleSvg} from "@/assets/svg";
+import Link from "next/link";
+
+export function RegisterForm({
+                              className,
+                              ...props
+                          }: React.ComponentPropsWithoutRef<"div">) {
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Registrar-se</CardTitle>
+                    <CardDescription>
+                        Cadastra-se para cadastrar informações do seu estabelecimento.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="name">Nome completo</Label>
+                                <Input
+                                    id="name"
+                                    type="text"
+                                    placeholder="Ex: João da Silva"
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="me@exemplo.com"
+                                />
+                            </div>
+
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Senha</Label>
+                                </div>
+                                <Input id="password" type="password" placeholder={"****************"}/>
+                            </div>
+                            <Button type="submit" className="w-full">
+                                Cadastrar
+                            </Button>
+                            <div
+                                className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                  Ou continue com
+                </span>
+                            </div>
+                            <Button variant="outline" className="w-full">
+                                {googleSvg}
+                                Registrar com Google
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Tem uma conta?{" "}
+                            <Link href="/login" className="underline underline-offset-4">
+                                Conecte-se
+                            </Link>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
